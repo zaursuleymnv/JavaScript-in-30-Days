a5fdc5be7dd7abb6e59c9e17159a22b09c0a15f3
zaursuleymnv <szaur018@mail.ru>
2023-02-02 17:20:54 +0100

    Introduction

---
+++ Day 1 of JS/js/main.js
@@ -0,0 +1,53 @@
+// console.log('Hello, Worlddd');
+
+// var variable1;
+// variable1 = 'Zaur';
+// variable1 = 100;
+// console.log(typeof variable1);
+
+// const serialKiller = {
+//     firstName: 'Dahmer', 
+//     lastName: 'Jeffrey', 
+//     age: 30,
+//     colorHair: "Blond",
+//     isStuying: false, 
+//     martialStatus: null
+// }
+
+// console.log(serialKiller.firstName, serialKiller.lastName + ' ' + serialKiller.age);
+
+// let person1 = {
+//     name:'',
+//     surname: 'Bundy',
+//     age:'',
+// }
+
+// console.log(person1.name);
+
+// let name = 'Elchin';
+// name = 'Zaur';
+// console.log(name);
+
+// let num1 = new Number('15');
+// num2 = new Number('60');
+// total = num1 + num2;
+
+// console.log(total.length);
+
+// let firstName = 'Zaur';
+// surName = 'Suleymanov'
+
+// console.log((firstName+ ' ' + surName).length);
+
+// let names = ['Zaur', 'Elchin', 'Dahmer', 'Eshref', 'Ted'];
+// names[2] = new Number;
+// console.log(typeof (names[2]));
+// console.log((names[2].toString) instanceof String);
+
+// let order1 = 340.76,
+//     order2 = 213.7;
+// totalOrder = parseInt(order1 + order2);
+// console.log(new String(totalOrder).length);
+
+const yearOfBirth = 2000;
+console.log('Your age is:', new Date().getFullYear() - yearOfBirth);
