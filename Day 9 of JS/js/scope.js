bc28d359c7aae77b510062dfdf52b3d28da50571
zaursuleymnv <szaur018@mail.ru>
2023-03-29 23:01:39 +0200

    Higher Order Functions

--- Day 9 of JS/js/scope.js
+++ Day 9 of JS/js/scope.js
@@ -390,6 +390,7 @@ console.log(names3);
 //                                            Exercises: Level 1
 
 const countries3 = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
+const countries4 = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
 const names5 = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
 const numbers8 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const products = [
@@ -422,10 +423,191 @@ numbers8.forEach(number => {
 const countryToUpperCase1 = countries3.map(country => country.toUpperCase())
 console.log(countryToUpperCase1);
 
+//Use map to create an array of countries length from countries array.
+const countryLength = countries3.map(country => country.length)
+console.log(countryLength);
+
+//Use map to create a new array by changing each number to square in the numbers array
+const numsquare1 = numbers8.map(num => Math.pow(num, 2))
+console.log(numsquare1);
+
+//Use map to change to each name to uppercase in the names array
+const nameToUpperCase1 = names5.map((name) => name.toUpperCase())
+console.log(nameToUpperCase1);
+
+//Use map to map the products array to its corresponding prices.
+const priceOfProduct1 = products.map(item => {
+  return {
+    productPrice: `The price of ${item.product} is ${item.price}`
+  }
+})
+console.log(priceOfProduct1);
+
+//Use filter to filter out countries containing land.
+// const countryIncludeLand1 = countries3.filter((country) => console.log(country.includes('land')))
+const searchContainFunction = (key, arr) => arr.filter((item) => item.toLowerCase().includes(key.toLowerCase()))
+console.log(searchContainFunction('land', countries3));
+
+//Use filter to filter out countries having six character.
+const countryLength2 = countries3.filter((country) => {
+  if (country.length === 6) {
+    return country
+  }
+})
+console.log(countryLength2);
+
+//Use filter to filter out countries containing six letters and more in the country array.
+const countryLength3 = countries3.filter((country) => {
+  if (country.length >= 6) {
+    return country
+  }
+})
+console.log(countryLength3);
+
+//Use filter to filter out country start with 'E';
+const searchStartWithFunction = (key, arr) => arr.filter((item) => item.toLowerCase().startsWith(key.toLowerCase()))
+console.log(searchStartWithFunction('E', countries3));
+
+//Use filter to filter out only prices with values.
+const productPrice2 = products.filter((val) => console.log(val.price))
+
+//Declare a function called getStringLists which takes an array as a parameter and then returns an array only with string items.
+function getStringLists(arr) {
+  return arr.filter(item => typeof item === 'string')
+}
+console.log(getStringLists(names5));
+
+//Use reduce to sum all the numbers in the numbers array. 
+const calculateTotal1 = numbers8.reduce((acc, cur) => acc + cur, 0)
+console.log(calculateTotal1);
+
+/* Use reduce to concatenate all the countries and to produce this sentence: 
+Estonia, Finland, Sweden, Denmark, Norway, and IceLand are north European countries
+*/
+
+// WAY 1
+const countrySentence1 = countries3.reduce((country) => {
+  // const formatter = new Intl.ListFormat('en', { 
+  //   style: 'long', 
+  //   type: 'conjunction' 
+  // });
+  
+  // let countries = formatter.format(country)
+  // let sentence = `${countries} north European countries`
+
+  // return sentence
+
+  let countryWithoutLastOne = countries3.splice(0, country.length -1)
+  .join(', ')
+  let lastCountry = countries3.splice(country.length -1)
+  let statement = `${countryWithoutLastOne} and ${lastCountry}`
+  let sentence =  `${statement} are north European countries`
+
+  return sentence
+
+})
+console.log(countrySentence1);
+
+// Use some to check if some names' length greater than seven in names array
+const resultName1 = names5.some((name) => {
+  if (name.length > 7) {
+    return name
+  }
+})
+console.log(resultName1);
+
+//Use every to check if all the countries contain the word land
+const searchIncludeLand = (key, arr) => arr.every(item => item.toLowerCase().includes(key.toLowerCase()))
+console.log(searchIncludeLand('land', countries4));
+
+//Use find to find the first country containing only six letters in the countries array
+const countryLength4 = countries4.find((country) => country.length === 6)
+console.log(countryLength4);
+
+//Use findIndex to find the position of the first country containing only six letters in the countries array
+const countryLength5 = countries4.findIndex((country) => country.length === 6)
+console.log(countryLength5);
+
+//Use findIndex to find the position of Norway if it doesn't exist in the array you will get -1.
+// const resultCountryPosition = countries4.findIndex((country) => country.toLowerCase() === 'Norway'.toLowerCase())
+// console.log(resultCountryPosition);
+
+const searchCountryPosition = (key, arr) => arr.findIndex((country) => country.toLowerCase() === key.toLowerCase())
+console.log(searchCountryPosition('Norway', countries4));
+
+// Use findIndex to find the position of Russia if it doesn't exist in the array you will get -1.
+console.log(searchCountryPosition('Russia', countries4));
 
 
 
 
+// -----------------------------------------------------------------------------------------------------------------
 
+//                                            Exercises: Level 2
+
+/* Find the total price of products by chaining two or more array 
+iterators(eg. arr.map(callback).filter(callback).reduce(callback))
+*/
+const totalPrice1 = products
+  .filter(product => typeof product.price === 'number' && product.price != 0)
+  .map(product => product.price)
+  .reduce((acc, cur) => acc + cur, 0)
+
+console.log(totalPrice1);
+
+//Find the sum of price of products using only reduce reduce(callback))
+const totalPrice2 = products.reduce((acc, cur) => {
+  if(typeof cur.price === 'number' && cur.price != 0) {
+    return acc + cur.price
+  }
+  else {
+    return acc
+  }
+}, 0)
+console.log(totalPrice2);
+
+/* Declare a function called categorizeCountries which returns an array of countries which have some common 
+pattern(you find the countries array in this repository as countries.js(eg 'land', 'ia', 'island','stan')).
+*/
+import { countries5 } from "./countries.js";
+
+function categorizeCountries(countries) {
+  const patterns = {
+    land: /land$/i,
+    ia: /ia$/i,
+    island: /island$/i,
+    stan: /stan$/i,
+  };
+
+  const result = {};
+  
+  for (const patternKey in patterns) {
+    const pattern = patterns[patternKey];
+    result[patternKey] = countries.filter((country) => pattern.test(country));
+  }
+
+  return result;
+}
+console.log(categorizeCountries(countries5));
+
+/* Create a function which return an array of objects, 
+which is the letter and the number of times the letter use to start with a name of a country.
+*/
+function countStartingLetters(countries) {
+  const startingLetters = {};
+  
+  for (const country of countries) {
+    const firstLetter = country.charAt(0);
+    
+    if (!startingLetters[firstLetter]) {
+      startingLetters[firstLetter] = 1;
+    } else {
+      startingLetters[firstLetter]++;
+    }
+  }
+  
+  return Object.entries(startingLetters).map(([letter, count]) => ({ letter, count }));
+}
+console.log(countStartingLetters(countries5));
 
 
